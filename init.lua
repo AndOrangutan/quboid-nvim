edbd1fc915664c66aa125ac69516095fb0017d2e
Von Mueller <vonjonmueller@gmail.com>
2023-04-13 13:23:13 -0700

    Setup basic init.lua with load structure

---
+++ init.lua
@@ -0,0 +1,16 @@
+-- Stage 0 - Oh wait this is lua
+
+-- Stage 1 - Load lua defualts and global settings
+
+require('quboid')
+
+require('defaults.options')
+require('defaults.autocmds')
+require('defaults.keymaps')
+-- require("defaults.mouse")
+
+-- Stage 2 - Load plugins
+require('plugin-management')      -- Load plugin manager
+
+-- Stage 3 - Load plugin dependent files
+require('colorscheme')
