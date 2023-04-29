ab9917676032550eb7d7c254ee84648f0001a7d2
Von Mueller <vonjonmueller@gmail.com>
2023-04-29 03:40:00 -0700

    Add mappings to open current file in browser

--- lua/defaults/keymaps.lua
+++ lua/defaults/keymaps.lua
@@ -4,6 +4,9 @@ local bufdelete_ok, bufdelete = pcall(require, 'bufdelete')
 
 -- util.keymap('n', 'p', 'p=`]', '[p]aste and auto format')
 
+util.keymap('n', '<F12>f', '<cmd>exe ":silent !firefox %"<cr>', 'Open current file in Firefox')
+util.keymap('n', '<F12>c', '<cmd>exe ":silent !chromium %"<cr>', 'Open current file in Chromium')
+
 util.keymap('n', 'Q', function ()
    local write_ok, write = pcall(vim.api.nvim_command, 'write')
    if not write_ok then
