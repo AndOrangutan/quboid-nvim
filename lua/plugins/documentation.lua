28896ba8cfe1d4cf5c20bedcd7b788b0f3bc5d39
Von Mueller <vonjonmueller@gmail.com>
2023-05-15 03:02:13 -0700

    Config Neodocs with `gcd`

--- lua/plugins/documentation.lua
+++ lua/plugins/documentation.lua
@@ -1,10 +1,13 @@
+
+local util = require('util')
 return {
     { 'danymat/neogen',
         dependencies = 'nvim-treesitter/nvim-treesitter',
         config = function()
             local neogen = require('neogen')
-            require('neogen').setup({})
+            require('neogen').setup({ input_after_comment = true })
             neogen.setup({ snippet_engine = 'luasnip' })
+            util.keymap('n', 'gcd', function () require('neogen').generate() end, '[c]omment [d]ocumentation')
         end,
     },
 }
