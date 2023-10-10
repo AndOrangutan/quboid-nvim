7d77ba65e1cd4d809bf3d1d8982675dfa17429ce
Von Mueller <vonjonmueller@gmail.com>
2023-10-10 00:35:32 -0700

    Fix some lsp config issues

--- lua/lsp/util.lua
+++ lua/lsp/util.lua
@@ -40,39 +40,12 @@ M.set_keymaps = function (_, bufnr)
     -- util.keymap('n', '<leader>ca', '<cmd>CodeActionMenu<cr>', { desc = 'Lsp [c]ode [a]ction', buffer = bufnr})
     vim.keymap.set('n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<cr>', { desc = 'Lsp [c]ode [a]ction', buffer = bufnr})
 
-    local function openHelpLinks(client, bufnr)
-    end 
-
-    -- Add keymaps for opening links.
-    if not vim.b[buf].markdown_keys then
-        vim.keymap.set('n', 'K', function()
-            -- Vim help links.
-            local url = (vim.fn.expand '<cWORD>' --[[@as string]]):match '|(%S-)|'
-            if url then
-                return vim.cmd.help(url)
-            end
-
-            -- Markdown links.
-            local col = vim.api.nvim_win_get_cursor(0)[2] + 1
-            local from, to
-            from, to, url = vim.api.nvim_get_current_line():find '%[.-%]%((%S-)%)'
-            if from and col >= from and col <= to then
-                vim.system({ 'open', url }, nil, function(res)
-                    if res.code ~= 0 then
-                        vim.notify('Failed to open URL' .. url, vim.log.levels.ERROR)
-                end
-            end)
-        end
-        end, { buffer = buf, silent = true })
-        vim.b[buf].markdown_keys = true
-    end
 
 end
 
-
 M.call_on_attach = function (client, bufnr)
 
-
+    local methods = vim.lsp.protocol.Methods
     vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
     -- Autoformat
@@ -93,31 +66,99 @@ M.call_on_attach = function (client, bufnr)
     end
 
     -- Extra highlights.
-    for l, line in ipairs(vim.api.nvim_buf_get_lines(buf, 0, -1, false)) do
-        for pattern, hl_group in pairs {
-            ['|%S-|'] = '@text.reference',
-            ['@%S+'] = '@parameter',
-            ['^%s*(Parameters:)'] = '@text.title',
-            ['^%s*(Return:)'] = '@text.title',
-            ['^%s*(See also:)'] = '@text.title',
-            ['{%S-}'] = '@parameter',
-        } do
-            local from = 1 ---@type integer?
-            while from do
-                local to
-                from, to = line:find(pattern, from)
-                if from then
-                    vim.api.nvim_buf_set_extmark(buf, md_namespace, l - 1, from - 1, {
-                        end_col = to,
-                        hl_group = hl_group,
-                    })
+
+    local md_namespace = vim.api.nvim_create_namespace 'mariasolos/lsp_float'
+
+    ---LSP handler that adds extra inline highlights, keymaps, and window options.
+    ---Code inspired from `noice`.
+    ---@param handler fun(err: any, result: any, ctx: any, config: any): integer, integer
+    ---@return function
+    local function enhanced_float_handler(handler)
+        return function(err, result, ctx, config)
+            local buf, win = handler(
+                err,
+                result,
+                ctx,
+                vim.tbl_deep_extend('force', config or {}, {
+                    border = quboid.border_float,
+                    max_height = math.floor(vim.o.lines * 0.5),
+                    max_width = math.floor(vim.o.columns * 0.4),
+                })
+            )
+
+            if not buf or not win then
+                return
+            end
+
+            -- Conceal everything.
+            vim.wo[win].concealcursor = 'n'
+
+            -- Extra highlights.
+            for l, line in ipairs(vim.api.nvim_buf_get_lines(buf, 0, -1, false)) do
+                for pattern, hl_group in pairs {
+                    ['|%S-|'] = '@text.reference',
+                    ['@%S+'] = '@parameter',
+                    ['^%s*(Parameters:)'] = '@text.title',
+                    ['^%s*(Return:)'] = '@text.title',
+                    ['^%s*(See also:)'] = '@text.title',
+                    ['{%S-}'] = '@parameter',
+                } do
+                    local from = 1 ---@type integer?
+                    while from do
+                        local to
+                        from, to = line:find(pattern, from)
+                        if from then
+                            vim.api.nvim_buf_set_extmark(buf, md_namespace, l - 1, from - 1, {
+                                end_col = to,
+                                hl_group = hl_group,
+                            })
+                        end
+                        from = to and to + 1 or nil
+                    end
                 end
-                from = to and to + 1 or nil
+            end
+
+            -- Add keymaps for opening links.
+            if not vim.b[buf].markdown_keys then
+                vim.keymap.set('n', 'K', function()
+                    -- Vim help links.
+                    local url = (vim.fn.expand '<cWORD>' --[[@as string]]):match '|(%S-)|'
+                    if url then
+                        return vim.cmd.help(url)
+                    end
+
+                    -- Markdown links.
+                    local col = vim.api.nvim_win_get_cursor(0)[2] + 1
+                    local from, to
+                    from, to, url = vim.api.nvim_get_current_line():find '%[.-%]%((%S-)%)'
+                    if from and col >= from and col <= to then
+                        vim.system({ 'open', url }, nil, function(res)
+                            if res.code ~= 0 then
+                                vim.notify('Failed to open URL' .. url, vim.log.levels.ERROR)
+                            end
+                        end)
+                    end
+                end, { buffer = buf, silent = true })
+                vim.b[buf].markdown_keys = true
             end
         end
     end
 
+    vim.lsp.handlers[methods.textDocument_hover] = enhanced_float_handler(vim.lsp.handlers.hover)
+    vim.lsp.handlers[methods.textDocument_signatureHelp] = enhanced_float_handler(vim.lsp.handlers.signature_help)
+    vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with( vim.lsp.diagnostic.on_publish_diagnostics, {
+        virtual_text = false,
 
+        -- update_in_insert = false,
+        -- virtual_lines = { only_current_line = true },
+        -- -- signs = true,
+        -- underline = false,
+        -- -- update_in_insert = false,
+        -- severity_sort = true,
+        -- float = {
+        --     source = "always",
+        -- },
+    })
 end
 
 M.gen_capabilities = function (ext_capabilities)
