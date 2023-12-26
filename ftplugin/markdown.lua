1365c00b567efb26701234e0a1d3150342202d80
Von Mueller <vonjonmueller@gmail.com>
2023-12-26 01:09:12 -0800

    Config markdown for hard wraping

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -1,8 +1,10 @@
 
-vim.o.textwidth = 0
-vim.opt_local.wrap = true
+vim.opt_local.textwidth = 80
+vim.opt_local.wrap = false
 vim.opt_local.spell = false
 vim.opt_local.linebreak = true
+vim.opt_local.comments="fb:>,fb:*,fb:+,fb:-,fb::"
+vim.opt_local.formatoptions = "tacqwn"
 
 vim.keymap.set('n', 'j', 'gj')
 vim.keymap.set('n', 'k', 'gk')
