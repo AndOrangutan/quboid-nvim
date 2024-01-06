edeac0c0b5528c5f077d20c376ccfbc896e36d4e
Von Mueller <vonjonmueller@gmail.com>
2024-01-06 04:23:28 -0800

    Add action picker for code action menu

--- lua/plugins/picker.lua
+++ lua/plugins/picker.lua
@@ -2,12 +2,24 @@ return {
     { 'stevearc/dressing.nvim',
         opts = {
             input = {
-                override = function(conf)
-                    conf.col = -1
-                    conf.row = 0
-                    return conf
-                end,
+                border = require('quboid').border,
+                -- override = function(conf)
+                --     conf.col = -1
+                --     conf.row = 0
+                --     return conf
+                -- end,
             },
+            select = {
+                telescope = require('telescope.themes').get_ivy({
+                            borderchars = {
+                                prompt =  { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' };
+                                results = { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' };
+                                preview = { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' };
+                            },
+
+                })
+            },
+            telescope = require('telescope.themes').get_dropdown()
         },
     },
     { 'nvim-telescope/telescope.nvim',
@@ -16,6 +28,25 @@ return {
             { 'nvim-telescope/telescope-fzf-native.nvim',
                 build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build'
             },
+			{ 'aznhe21/actions-preview.nvim',
+				config = function ()
+					require("actions-preview").setup {
+						telescope = {
+							sorting_strategy = "ascending",
+							layout_strategy = "vertical",
+							layout_config = {
+								width = 0.95,
+								height = 0.95,
+								prompt_position = "top",
+								preview_cutoff = 20,
+								preview_height = function(_, _, max_lines)
+									return max_lines - 15
+								end,
+							},
+						},
+					}
+				end,
+			},
         },
         config = function()
             local telescope = require('telescope')
@@ -29,18 +60,20 @@ return {
                         }
                     }, 
                     vimgrep_arguments = {
-                        "rg",
-                        "--color=never",
-                        "--no-heading",
-                        "--with-filename",
-                        "--line-number",
-                        "--column",
-                        "--smart-case",
-                        "--trim" -- add this value
+                        'rg',
+                        '--color=never',
+                        '--no-heading',
+                        '--no-ignore',
+                        '--with-filename',
+                        '--line-number',
+                        '--column',
+                        '--smart-case',
+                        '--trim' -- add this value
                     },
-                    prompt_prefix = "   ",
-                    selection_caret = "  ",
-                    entry_prefix = " ",
+                    file_pattern_ignore = require('quboid').exclude_patterns,
+                    prompt_prefix = ' '..require('quboid').icons.search,
+                    selection_caret = '  ',
+                    entry_prefix = ' ',
                     initial_mode = "insert",
                     selection_strategy = "reset",
                     sorting_strategy = "ascending",
@@ -53,13 +86,18 @@ return {
                         },
                         vertical = {
                             mirror = false,
+
                         },
                         width = 0.95,
                         height = 0.95,
                         preview_cutoff = 120,
                     },
-                    border = {},
-                    borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
+                    winblend = require('quboid').winblend,
+                    borderchars = {
+                        prompt =  { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' };
+                        results = { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' };
+                        preview = { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' };
+                    },
                 },
                 pickers = {
                 },
@@ -69,16 +107,16 @@ return {
                         override_generic_sorter = true,  -- override the generic sorter
                         override_file_sorter = true,     -- override t ffhe file sorter
                         case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
-                        -- the default case_mode is "smart_case"
                     },
                 }
             }
 
             telescope.load_extension('fzf')
             telescope.load_extension('git_worktree')
+            telescope.load_extension('zk')
         end,
         keys = {
-            { '<leader>ff', '<cmd>Telescope find_files<cr>', desc = 'Telescope [f]Zf [f]iles' },
+            { '<leader>ff', '<cmd>Telescope find_files hidden=true no_ignore=true<cr>', desc = 'Telescope [f]Zf [f]iles' },
             { '<leader>fg', '<cmd>Telescope live_grep<cr>', desc = 'Telescope [f]ZF [g]rep' },
             { '<leader>fa', '<cmd>Telescope builtin<cr>', desc = 'Telescope [f]ZF [a]ll' },
             { '<leader>fc', '<cmd>Telescope colorscheme<cr>', desc = 'Telescope [f]ZF [c]olorscheme' },
@@ -91,6 +129,6 @@ return {
             -- { '<leader>fh', [[<cmd>lua require("require('telescope.builtin')").help_tags<cr>]], desc = 'Telescope [f]ZF [h]elp Tags' },
 
         },
-        -- event = { 'BufReadPre', 'BufNewFile' },
+        event = { 'BufReadPre', 'BufNewFile' },
     },
 }
