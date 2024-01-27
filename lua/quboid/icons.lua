f7a7c22b70ddcd8c0a4ad40dbf3431c325a4c5ca
Von Mueller <vonjonmueller@gmail.com>
2024-01-27 14:35:21 -0800

    Add icons file

---
+++ lua/quboid/icons.lua
@@ -0,0 +1,34 @@
+local M = {}
+
+-- General icons for use throughout the editor
+M.gen = {
+    editor = ' ',
+
+
+
+    bolt        = ' ',
+    check       = ' ',
+    circle      = ' ',
+    circle_half = ' ',
+    circle_o    = ' ',
+    cmd         = ' ',
+    code        = ' ',
+    cog         = '󰒓 ',
+    file        = ' ',
+    file_export = ' ',
+    file_inport = ' ',
+    keybord     = ' ',
+    pause       = ' ',
+    play        = ' ',
+    plug        = ' ',
+    zzz         = '󰒲 ',
+}
+M.lang = {
+    lua = ' ',
+}
+
+M.ui = {
+
+}
+
+return M
