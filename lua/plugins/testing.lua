9f4346cac40995f467eb2ea6f1566d45e7a5f1e9
Von Mueller <vonjonmueller@gmail.com>
2024-02-12 00:23:57 -0800

    Add keybinds to neotest, need to test :p

--- lua/plugins/testing.lua
+++ lua/plugins/testing.lua
@@ -12,5 +12,32 @@ return {
                 },
             })
         end,
+        keys = {
+            {
+                '<leader>trn',
+                '<cmd>lua require("neotest").run.run()<cr>',
+                desc =
+                'Neo[t]est [r]un [n]earest'
+            },
+            {
+                '<leader>tr%',
+                '<cmd>lua require("neotest").run.run(vim.fn.expand("%"))<cr>',
+                desc =
+                'Neo[t]est [r]un [c]ur. File'
+            },
+            {
+                '<leader>trd',
+                '<cmd>lua require("neotest").run.run({strategy = "dap"})<cr>',
+                desc =
+                'Neo[t]est [r]un [c]ur. File'
+            },
+            {
+                '<leader>tt',
+                '<cmd>lua require("neotest").run.stop()<cr>',
+                desc =
+                'Neo[t]est [s]top Nearest'
+            },
+            { '<leader>ta', '<cmd>lua require("neotest").run.attach()<cr>', desc = 'Neo[t]est [a]ttach Nearest' },
+        },
     }
 }
